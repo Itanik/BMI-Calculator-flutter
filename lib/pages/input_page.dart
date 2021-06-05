a14349ac63d58fe2bf016f8b83af82eaae6ee77f
Nikita Ulyantsev <nikita.ulyancev@compvisionsys.com>
2021-06-05 19:55:28 +0300

    Configure gender card design.

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -6,6 +6,7 @@ const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
 const inactiveCardColor = Color(0xFF111428);
 const bottomContainerColor = Color(0xFFEB1555);
+const genderTextColor = Color(0xFF8D8E98);
 
 class InputPage extends StatefulWidget {
   @override
@@ -64,7 +65,21 @@ class _InputPageState extends State<InputPage> {
   Widget buildGenderButton(IconData genderIcon, String genderText) => Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [Icon(genderIcon), Text(genderText)],
+        children: [
+          Icon(
+            genderIcon,
+            size: 80.0,
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Center(
+            child: Text(
+              genderText,
+              style: TextStyle(fontSize: 18.0, color: genderTextColor),
+            ),
+          )
+        ],
       );
 
   Widget buildHeightSpinner() => Column(
