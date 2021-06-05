3a38a5536cbce04565629df2c38d0d35796ecf5a
Nikita Ulyantsev <nikita.ulyancev@compvisionsys.com>
2021-06-05 19:27:01 +0300

    Prepare content.

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const bottomContainerHeight = 80.0;
@@ -24,22 +25,28 @@ class _InputPageState extends State<InputPage> {
             children: [
               ExpandedCard(
                 bgColor: activeCardColor,
+                child: buildGenderButton(Icons.male, "Male"),
               ),
               ExpandedCard(
                 bgColor: activeCardColor,
+                child: buildGenderButton(Icons.female, "Female"),
               ),
             ],
           )),
           ExpandedCard(
+            child: buildHeightSpinner(),
             bgColor: inactiveCardColor,
           ),
           Expanded(
               child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               ExpandedCard(
+                child: buildCounterCard("Weight"),
                 bgColor: inactiveCardColor,
               ),
               ExpandedCard(
+                child: buildCounterCard("Age"),
                 bgColor: inactiveCardColor,
               ),
             ],
@@ -52,16 +59,55 @@ class _InputPageState extends State<InputPage> {
           )
         ],
       ));
+
+  Widget buildGenderButton(IconData genderIcon, String genderText) => Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Icon(genderIcon), Text(genderText)],
+      );
+
+  Widget buildHeightSpinner() => Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Height"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("183"),
+              Text("cm"),
+            ],
+          ),
+          Text("spinner"),
+        ],
+      );
+
+  Widget buildCounterCard(String text) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(text),
+          Text("73"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.remove),
+              Icon(Icons.add),
+            ],
+          )
+        ],
+      );
 }
 
 class ExpandedCard extends StatelessWidget {
   final Color bgColor;
+  final Widget child;
 
-  ExpandedCard({required this.bgColor});
+  ExpandedCard({required this.bgColor, required this.child});
 
   @override
   Widget build(BuildContext context) => Expanded(
         child: Container(
+          child: child,
           margin: EdgeInsets.all(12.0),
           decoration: BoxDecoration(
             color: bgColor,
